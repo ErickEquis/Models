038541edc689a8f44d73b443b5994117e48adc00
ErickEquis <ericksantillan95.01@gmail.com>
2023-08-20 14:09:52 -0600

    fix

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
-from apiBanxico import apiBanxico
+from routes.api.apiBanxico import apiBanxico
 
 app = Flask(__name__)
 
 @app.route("/banxico/<idSerie>")
 def banxico(idSerie):
-
     return apiBanxico(idSerie)
+
+@app.route("/")
+def home():
+    return "Hola Mundo!"
